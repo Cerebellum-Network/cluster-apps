5cacdd49ca64feca25899b738b8d72adf91a7f3a
antonmazhuto <anton.mazhuto@cere.io>
2024-07-18 17:24:42 +0300

    Improved components

--- src/components/layouts/onboarding/OnboardingVideoSlider/OnboardingVideoSlider.styled.tsx
+++ src/components/layouts/onboarding/OnboardingVideoSlider/OnboardingVideoSlider.styled.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import {Typography} from '@mui/material';
+import { Typography } from '@mui/material';
 
 export const SliderDot = styled('div')({
   width: '8px',
