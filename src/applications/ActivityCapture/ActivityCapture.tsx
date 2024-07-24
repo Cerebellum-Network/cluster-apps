c6abe0f4d2b64aca5c98ed8867f2b7ca24d2bfa7
Sergey Kambalin <sergey.kambalin@cere.io>
2024-07-24 19:57:19 +0600

    chore: Customer Data Platform - coming soon

--- src/applications/ActivityCapture/ActivityCapture.tsx
+++ src/applications/ActivityCapture/ActivityCapture.tsx
@@ -1,6 +1,6 @@
 import { Typography } from '@developer-console/ui';
 import { observer } from 'mobx-react-lite';
 
-const ActivityCapture = () => <Typography>Activity Capture</Typography>;
+const ActivityCapture = () => <Typography>Coming Soon...</Typography>;
 
 export default observer(ActivityCapture);
