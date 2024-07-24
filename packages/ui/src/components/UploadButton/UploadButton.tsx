56991ce8ec404388e66cf9d9b17c41e1a701adfa
antonmazhuto <anton.mazhuto@cere.io>
2024-07-24 12:53:27 +0300

    Added folder upload

--- packages/ui/src/components/UploadButton/UploadButton.tsx
+++ packages/ui/src/components/UploadButton/UploadButton.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { ListItemIcon, MenuItem, MenuList, Typography } from '@mui/material';
-import { Dropdown, UploadFileIcon } from '@developer-console/ui';
+import { Dropdown, UploadFileIcon, UploadFolderIcon } from '@developer-console/ui';
 
 interface UploadComponentProps {
   onDrop: (values: { acceptedFiles: File[]; bucketId: string; cnsName: string; isFolder: boolean }) => void;
@@ -23,24 +23,22 @@ export const UploadButton = ({ bucketId, cnsName, onDrop }: UploadComponentProps
     setOpen((prevState) => !prevState);
   };
 
-  // @TODO uncomment when folder uploading will be ready
-  // const handleUploadFolder = () => {
-  //   const input = document.createElement('input');
-  //   input.type = 'file';
-  //   input.webkitdirectory = true;
-  //   input.multiple = true;
-  //   input.onchange = (e: Event) => {
-  //     const target = e.target as HTMLInputElement;
-  //     const files = Array.from(target.files || []);
-  //     onDrop({ acceptedFiles: files, bucketId, cnsName, isFolder: true });
-  //   };
-  //   input.click();
-  //   setOpen((prevState) => !prevState);
-  // };
+  const handleUploadFolder = () => {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.webkitdirectory = true;
+    input.multiple = true;
+    input.onchange = (e: Event) => {
+      const target = e.target as HTMLInputElement;
+      const files = Array.from(target.files || []);
+      onDrop({ acceptedFiles: files, bucketId, cnsName, isFolder: true });
+    };
+    input.click();
+    setOpen((prevState) => !prevState);
+  };
 
   return (
     <Dropdown open={openDropdown} onToggle={setOpen} label="Upload">
-      {/*<div {...getRootProps()}>*/}
       <MenuList>
         <MenuItem
           onClick={(event) => {
@@ -54,19 +52,18 @@ export const UploadButton = ({ bucketId, cnsName, onDrop }: UploadComponentProps
           </ListItemIcon>
           <Typography>Upload File</Typography>
         </MenuItem>
-        {/*@TODO uncomment when folder uploading will be ready*/}
-        {/*<MenuItem*/}
-        {/*  onClick={(event) => {*/}
-        {/*    event.stopPropagation();*/}
-        {/*    handleUploadFolder();*/}
-        {/*  }}*/}
-        {/*  disableRipple*/}
-        {/*>*/}
-        {/*  <ListItemIcon>*/}
-        {/*    <UploadFolderIcon fontSize="small" />*/}
-        {/*  </ListItemIcon>*/}
-        {/*  <Typography>Upload Folder</Typography>*/}
-        {/*</MenuItem>*/}
+        <MenuItem
+          onClick={(event) => {
+            event.stopPropagation();
+            handleUploadFolder();
+          }}
+          disableRipple
+        >
+          <ListItemIcon>
+            <UploadFolderIcon fontSize="small" />
+          </ListItemIcon>
+          <Typography>Upload Folder</Typography>
+        </MenuItem>
       </MenuList>
     </Dropdown>
   );
