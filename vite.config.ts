d3e83586821c2a4d9380eab0ec4889bfce00efd4
antonmazhuto <anton.mazhuto@cere.io>
2024-07-29 16:47:40 +0300

    Fix login text

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 /**
  * For more information, visit: https://vitejs.dev/config
@@ -18,13 +17,5 @@ export default defineConfig({
         Buffer: true,
       },
     }),
-    // viteStaticCopy({
-    //   targets: [
-    //     {
-    //       src: 'src/assets/*',
-    //       dest: 'assets',
-    //     },
-    //   ],
-    // }),
   ],
 });
