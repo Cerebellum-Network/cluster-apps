8f6227955599abfdaf2f4c8acba040b299aa2193
Sergey Kambalin <sergey.kambalin@cere.io>
2024-07-30 13:08:48 +0600

    docs: Review and update existing guides and remove unused

--- src/applications/ContentDelivery/docs/index.tsx
+++ src/applications/ContentDelivery/docs/index.tsx
@@ -1,11 +1,9 @@
 import { Markdown } from '@developer-console/ui';
 
-import startGuide from './startGuide.md?raw';
 import success from './success.md?raw';
 import stream from './stream.md?raw';
 import fileUploadStepByStep from './fileUploadStepByStep.md?raw';
 
-export const StartGuideDoc = () => <Markdown content={startGuide} />;
 export const SuccessDoc = () => <Markdown content={success} />;
 export const StreamDoc = () => <Markdown content={stream} />;
 export const UploadWithCliDoc = () => <Markdown content={fileUploadStepByStep} />;
--- src/applications/ContentDelivery/docs/startGuide.md
+++
@@ -1,3 +0,0 @@
-#### Start guide
-
-TBA
