b5e8a70f8123da5b71796794b77a42b389e3d06e
antonmazhuto <anton.mazhuto@cere.io>
2024-08-01 18:04:25 +0300

    Fixed form

--- src/routes/Login/Login.tsx
+++ src/routes/Login/Login.tsx
@@ -64,49 +64,51 @@ const Login = observer(() => {
 
   return (
     <OnboardingLayout>
-      <StyledStack justifyContent="center" alignItems="center" flex="1" onSubmit={onSubmit}>
-        <Typography variant="h1" textAlign="center">
-          Welcome to {DDC_CLUSTER_NAME}
-        </Typography>
-        <Typography textAlign="center" sx={{ mt: 4, mb: 3 }}>
-          Unlock the power of the first Web3 Data Cloud for real-world applications. <br />
-          Get started for free in just a minutes, no credit card required.
-        </Typography>
+      <form onSubmit={onSubmit}>
+        <StyledStack>
+          <Typography variant="h1" textAlign="center">
+            Welcome to {DDC_CLUSTER_NAME}
+          </Typography>
+          <Typography textAlign="center" sx={{ mt: 4, mb: 3 }}>
+            Unlock the power of the first Web3 Data Cloud for real-world applications. <br />
+            Get started for free in just a minutes, no credit card required.
+          </Typography>
 
-        <FormControl fullWidth>
-          <StyledTextField
-            {...register('email')}
-            type="email"
-            label="Account Email"
-            placeholder="Enter your email"
-            variant="outlined"
-            error={!!errors?.['email']?.message}
-          />
-        </FormControl>
-        <LoadingButton
-          fullWidth
-          type="submit"
-          variant="contained"
-          size="large"
-          disabled={!isValid}
-          loading={isSubmitting}
-          endIcon={<RightArrowIcon />}
-        >
-          Get Started
-        </LoadingButton>
+          <FormControl fullWidth>
+            <StyledTextField
+              {...register('email')}
+              type="email"
+              label="Account Email"
+              placeholder="Enter your email"
+              variant="outlined"
+              error={!!errors?.['email']?.message}
+            />
+          </FormControl>
+          <LoadingButton
+            fullWidth
+            type="submit"
+            variant="contained"
+            size="large"
+            disabled={!isValid}
+            loading={isSubmitting}
+            endIcon={<RightArrowIcon />}
+          >
+            Get Started
+          </LoadingButton>
 
-        <Terms textAlign="center" variant="body2">
-          By using your Cere wallet you automatically agree to our
-          <br />{' '}
-          <a href={TERMS_AND_CONDITIONS_LINK} target="_blank" rel="noopener noreferrer">
-            Terms & Conditions
-          </a>{' '}
-          and{' '}
-          <a href={PRIVACY_POLICY} target="_blank" rel="noopener noreferrer">
-            Privacy Policy
-          </a>
-        </Terms>
-      </StyledStack>
+          <Terms textAlign="center" variant="body2">
+            By using your Cere wallet you automatically agree to our
+            <br />{' '}
+            <a href={TERMS_AND_CONDITIONS_LINK} target="_blank" rel="noopener noreferrer">
+              Terms & Conditions
+            </a>{' '}
+            and{' '}
+            <a href={PRIVACY_POLICY} target="_blank" rel="noopener noreferrer">
+              Privacy Policy
+            </a>
+          </Terms>
+        </StyledStack>
+      </form>
     </OnboardingLayout>
   );
 });
