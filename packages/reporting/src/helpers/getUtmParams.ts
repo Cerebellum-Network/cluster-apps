d6a430d83cfbe50e32f37a9ab3fd1a6878c81c44
antonmazhuto <anton.mazhuto@cere.io>
2024-08-13 14:50:51 +0300

    Track utm params

--- packages/reporting/src/helpers/getUtmParams.ts
+++ packages/reporting/src/helpers/getUtmParams.ts
@@ -2,7 +2,7 @@ export const getUTMParameters = () => {
   const urlParams = new URLSearchParams(window.location.search);
   const utmParams: Record<string, string> = {};
 
-  ['utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content'].forEach((key) => {
+  ['utm_source', 'utm_medium', 'utm_campaign'].forEach((key) => {
     const value = urlParams.get(key);
     if (value) {
       utmParams[key] = value;
