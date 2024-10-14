68a50edd06b9bea5a480ad3139f04a91ae6336a1
antonmazhuto <anton.mazhuto@cere.io>
2024-10-14 23:16:37 +0300

    Fixed errors

--- apps/node-provider/src/hooks/index.ts
+++ apps/node-provider/src/hooks/index.ts
@@ -3,5 +3,4 @@ export * from './useAccountStore';
 export * from './useOnboardingStore';
 export * from './useQuestsStore';
 export * from './useAccount';
-export * from './useFetchDirs';
 export * from './use-email-compaign-service';
