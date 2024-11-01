7acc374368ddacb9854d6e967ef0a6f5233aeada
Anton Mazhuto <125869076+mazhutoanton@users.noreply.github.com>
2024-11-01 14:17:02 +0300

    Added telemetry agent run (#126)

--- apps/node-provider/bootstrap.sh
+++ apps/node-provider/bootstrap.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# Check for installed dependencies
 if ! command -v docker &> /dev/null || ! command -v docker-compose &> /dev/null; then
     echo "Docker and Docker Compose must be installed."
     exit 1
 fi
 
-if [ "$#" -ne 7 ]; then
-    echo "Usage: $0 STORAGE_ROOT BLOCKCHAIN_URL MODE nodeType port grpcPort p2pPort"
+# Check the number of arguments
+if [ "$#" -ne 9 ]; then
+    echo "Usage: $0 STORAGE_ROOT BLOCKCHAIN_URL MODE NODE_TYPE PORT GRPC_PORT P2P_PORT NETWORK_NAME CLUSTER_ID"
     exit 1
 fi
 
+# Set variables from arguments
 export STORAGE_ROOT=$1
 export BLOCKCHAIN_URL=$2
 export MODE=$3
@@ -17,6 +20,8 @@ export NODE_TYPE=$4
 export PORT=$5
 export GRPC_PORT=$6
 export P2P_PORT=$7
+export NETWORK_NAME=$8
+export CLUSTER_ID=$9
 
 # Generating a seed phrase and public key
 output=$(docker run --rm parity/subkey generate --scheme Ed25519)
@@ -24,10 +29,27 @@ output=$(docker run --rm parity/subkey generate --scheme Ed25519)
 export PEER_SECRET_PHRASE=$(echo "$output" | grep -o 'Secret phrase `[^`]*`' | sed 's/Secret phrase `//;s/`//')
 export PUBLIC_KEY=$(echo "$output" | grep -o 'Public key (hex): .*' | sed 's/Public key (hex): //')
 
-# Output public key
-echo "Generated public key: $PUBLIC_KEY"
+# Use PUBLIC_KEY as NODE_ID
+export NODE_ID=$PUBLIC_KEY
 
-echo "Running node..."
+# Output public key and NODE_ID
+echo "Generated public key: $PUBLIC_KEY"
+echo "Using NODE_ID: $NODE_ID"
+
+# Path to agent configuration file
+AGENT_CONFIG_FILE="./agent-config.yaml"
+
+# Check for the existence of the configuration file
+if [ ! -f "$AGENT_CONFIG_FILE" ]; then
+    echo "Agent configuration file not found: $AGENT_CONFIG_FILE"
+    exit 1
+fi
+
+# Substituting variables into the configuration file using envsubst
+envsubst < "$AGENT_CONFIG_FILE" > "${AGENT_CONFIG_FILE}.tmp" && mv "${AGENT_CONFIG_FILE}.tmp" "$AGENT_CONFIG_FILE"
+
+# Running containers
+echo "Running node and agent..."
 STORAGE_ROOT=$STORAGE_ROOT \
 BLOCKCHAIN_URL=$BLOCKCHAIN_URL \
 MODE=$MODE \
@@ -37,11 +59,14 @@ GRPC_PORT=$GRPC_PORT \
 P2P_PORT=$P2P_PORT \
 PEER_SECRET_PHRASE=$PEER_SECRET_PHRASE \
 PUBLIC_KEY=$PUBLIC_KEY \
+NODE_ID=$NODE_ID \
+NETWORK_NAME=$NETWORK_NAME \
+CLUSTER_ID=$CLUSTER_ID \
 docker-compose up -d
 
 if [ $? -eq 0 ]; then
-    echo "The node has been launched successfully."
+    echo "The node and agent have been launched successfully."
 else
-    echo "An error occurred while starting the node."
+    echo "An error occurred while starting the node and agent."
     exit 1
 fi
