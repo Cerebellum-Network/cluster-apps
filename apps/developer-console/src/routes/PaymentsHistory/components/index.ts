8275d8ef08b6c4276c569a76f72d0f451adc36f1
antonmazhuto <anton.mazhuto@cere.io>
2025-05-15 02:01:32 +0300

    fixed codestyle

--- apps/developer-console/src/routes/PaymentsHistory/components/index.ts
+++ apps/developer-console/src/routes/PaymentsHistory/components/index.ts
@@ -2,4 +2,4 @@ export { default as FilterSection } from './FilterSection';
 export { default as SummaryCards } from './SummaryCards';
 export { default as CostTrendsChart } from './CostTrendsChart';
 export { default as UsageByBucket } from './UsageByBucket';
-export { default as PaymentsTable } from './PaymentsTable'; 
+export { default as PaymentsTable } from './PaymentsTable';
