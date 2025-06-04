618d7b9ae542758d1eb6fdcde7ce7de43be68837
antonmazhuto <anton.mazhuto@cere.io>
2025-06-04 12:42:14 +0300

    removed log

--- apps/node-provider/src/components/AccountDropdown/AccountDropdown.tsx
+++ apps/node-provider/src/components/AccountDropdown/AccountDropdown.tsx
@@ -30,8 +30,6 @@ const AccountDropdown = () => {
 
   const handleClose = () => setOpen(false);
 
-  console.log(account);
-
   return (
     <Dropdown variant="header" open={open} onToggle={setOpen} label="Account" leftElement={<AvatarIcon />}>
       <Stack spacing={2} width={240}>
